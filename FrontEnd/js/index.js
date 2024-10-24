9d81533164b717932fac7f4f1483dc6ba8ae5ad4
Neary <kibishiigaming@gmail.com>
2024-10-24 14:21:39 +0200

    test de nouvels solution pour les filtres

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,12 +1,13 @@
+const API_URL = "http://localhost:5678/api"; // Centraliser l'URL de base
+
 //Recuperation du Works depuis l'API
 async function getWorks() {
     const url = "http://localhost:5678/api/works";
     try {
     const response = await fetch(url);
     if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
+    throw new Error(`Response status: ${response.status}`);
     }
-
     const json = await response.json();
     console.log(json);
     for(let i = 0; i < json.length; i++) {
@@ -16,13 +17,12 @@ async function getWorks() {
     console.error(error.message);
     }
 }
-getWorks();
 
+//Affichage de la galerie
 function setFigure(data) {
     const figure = document.createElement("figure");
     figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
 				<figcaption>${data.title}</figcaption>`
-
 document.querySelector(".gallery").append(figure);
 }
 
@@ -32,9 +32,8 @@ async function getCategories() {
     try {
     const response = await fetch(url);
     if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
+    throw new Error(`Response status: ${response.status}`);
     }
-
     const json = await response.json();
     console.log(json);
     for(let i = 0; i < json.length; i++) {
@@ -44,7 +43,6 @@ async function getCategories() {
     console.error(error.message);
     }
 }
-getCategories();
 
 //creation des filtres
 function setFilters(data) {
@@ -56,12 +54,13 @@ function setFilters(data) {
 document.querySelector(".filters").append(filters);
 }
 
-//Ajouter un événement de clic et filtres
-const allButtom = document.querySelector(".all");
-allButtom.addEventListener("click", () => getWorks());
+//Ajouter un événement de clic et filtre au bouton Tous
+const buttonAll = document.querySelector(".all");
+buttonAll.addEventListener("click", () => getWorks());
 
 function filterWorks(cat){
     console.log(cat)
 }
 
-
+getWorks();
+getCategories();
